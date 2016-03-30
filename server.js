b0da7fccf43216e114cea50fcc7c8884d41888de
C.T. Lin <chentsulin@gmail.com>
2016-03-30 13:04:17 +0800

    Upgraded modules to koa@next

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 import session from 'koa-session';
 import views from 'koa-views';
+import mount from 'koa-mount';
+import serve from 'koa-static';
 import convert from 'koa-convert';
 import finalHandler from './lib/finalHandler';
 import router from './router';
@@ -11,17 +13,14 @@ import router from './router';
 const app = new Koa();
 
 app.use(finalHandler());
-app.use(convert(views(`${__dirname}/views`, {
+app.use(views(`${__dirname}/views`, {
   map: {
-    html: 'nunjucks'
-  }
-})));
-app.use(async (ctx, next) => {
-  ctx.render = co.wrap(ctx.render);
-  await next();
-});
+    html: 'nunjucks',
+  },
+}));
 app.use(logger());
-app.use(convert(bodyParser()));
+app.use(mount('/static', serve('public')));
+app.use(bodyParser());
 app.keys = ['some secret hurr'];
 app.use(convert(session(app)));
 app
